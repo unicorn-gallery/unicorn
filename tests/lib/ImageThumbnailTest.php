b77f342f5f7b1d9b07de64f013436f6e1c4c332e
wale ayandiran <waleayandiran@wales-MacBook-Pro.local>
2018-12-05 19:16:10 +0100

    Added an actual image file to the test dir in lib/assets

--- tests/lib/ImageThumbnailTest.php
+++ tests/lib/ImageThumbnailTest.php
@@ -8,26 +8,26 @@ use Mre\Unicorn\lib\Image;
 
 class ImageThumbnailTest extends TestCase
 {
-  public function testImageMustBeOfValidExtension() {
-    $imageFile = Image::createThumbnail('/assets/img_avatar.png');
-    $extension = strtolower(strrchr($imageFile, '.'));
-    echo $extension;
-    switch ($extension) {
-      case '.jpg':
-        $this->assertEquals('.jpg',$extension);
-        break;
-      case '.jpeg':
-        $this->assertStringEqualsFile('.jpeg',$extension);
-        break;
-      case '.gif':
-        $this->assertStringEqualsFile('.gif',$extension);
-        break;
-      case '.png':
-        $this->assertStringEqualsFile('.png',$extension);
-        break;
-      default:
-        $this->assertTrue(false, $extension);
-        break;
-    }
+    public function testImageMustBeOfValidExtension() {
+        $imageFile = Image::createThumbnail('/assets/img_avatar.png');
+        $extension = strtolower(strrchr($imageFile, '.'));
+        echo $extension;
+        switch ($extension) {
+            case '.jpg':
+                $this->assertEquals('.jpg',$extension);
+                break;
+            case '.jpeg':
+                $this->assertStringEqualsFile('.jpeg',$extension);
+                break;
+            case '.gif':
+                $this->assertStringEqualsFile('.gif',$extension);
+                break;
+            case '.png':
+                $this->assertStringEqualsFile('.png',$extension);
+                break;
+            default:
+                $this->assertTrue(false, $extension);
+                break;
+        }
   }
 }
